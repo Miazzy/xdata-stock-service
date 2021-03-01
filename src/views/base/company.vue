4f3bbed2399f6516da45475290bfb085ea04ddc0
yunwisdom <qq695926920@163.com>
2021-03-01 15:32:12 +0800

    init

--- src/views/base/company.vue
+++ src/views/base/company.vue
@@ -1,17 +1,51 @@
 <template>
 <keep-alive>
+
     <div id="content" style="margin-top: 0px; overflow-x: hidden;">
+
         <header id="wx-header" style="overflow-x:hidden;">
-            <van-nav-bar title="设立管理" left-text="返回" left-arrow @click-left="returnBack" @click-right="searching">
+            <van-nav-bar title="设立公司" left-text="返回" left-arrow @click-left="returnBack" @click-right="searching">
                 <template #right>
                     <van-icon name="search" />
                 </template>
             </van-nav-bar>
         </header>
 
-        <section>
+        <div style="padding:0px 0.25rem; padding-bottom:1.5rem;">
+            <section style="box-shadow: 0 0.13333rem 0.2rem 0 rgb(0 0 0 / 10%); background-color: #fff; border-radius: 0.21333rem; padding: 0 0.42667rem; margin-top: 0.48rem; margin-bottom:0.40rem;">
+                <div id="weui-cells-flex" class="weui-cells" style="display:block;position:relative;">
+                    <div class="weui-cell-title" style="font-size:0.425rem;margin-top:0.30rem;margin-left:0.05rem;padding-top:0.25rem;">设立管理</div>
+                    <div class="flex-layout-content" id="scanCell" style="margin-top:0.15rem;">
+                        <van-row>
+                            <van-col span="6">
+                                <van-badge>
+                                    <div class="child" />
+                                </van-badge>
+                            </van-col>
+                            <van-col span="6">
+                                <van-badge>
+                                    <div class="child" />
+                                </van-badge>
+                            </van-col>
+                            <van-col span="6">
+                                <van-badge>
+                                    <div class="child" />
+                                </van-badge>
+                            </van-col>
+                            <van-col span="6">
+                                <van-badge>
+                                    <div class="child" />
+                                </van-badge>
+                            </van-col>
+                        </van-row>
+                    </div>
+                </div>
+                <div style="margin-top:0.05rem;height:0.05rem;">
+                    <span></span>
+                </div>
+            </section>
+        </div>
 
-        </section>
     </div>
 </keep-alive>
 </template>
@@ -38,10 +72,12 @@ export default {
         tabbar
     },
     setup(props, context) {
-        const { ctx } = getCurrentInstance();
-        const $store = useStore(); 
+        const {
+            ctx
+        } = getCurrentInstance();
+        const $store = useStore();
         const $router = useRouter();
-        const $route = useRoute(); 
+        const $route = useRoute();
         const active = ref("");
 
         const state = reactive({
@@ -59,7 +95,7 @@ export default {
             $router.push("/mine");
         };
 
-        const searching = ()=>{
+        const searching = () => {
             console.log('searching');
         };
 
@@ -112,4 +148,20 @@ export default {
 
 <style lang="scss" scoped>
 @import "./base.scss";
+
+.child {
+    width: 1.65rem;
+    height: 1.65rem;
+    background: #0f0f0f;
+    border-radius: 4px;
+    margin: 4px 5px 4px 5px;
+}
+
+.child:first-child {
+    width: 1.65rem;
+    height: 1.65rem;
+    background: #0f0f0f;
+    border-radius: 40px;
+    margin: 4px 5px 4px .225rem;
+}
 </style>
