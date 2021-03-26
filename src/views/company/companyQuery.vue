90f1fbfdd31caf174ce3e0a767fef57f916d0e3d
yunwisdom <qq695926920@163.com>
2021-03-26 15:33:39 +0800

    init

---
+++ src/views/company/companyQuery.vue
@@ -0,0 +1,377 @@
+<template>
+<div class="home">
+    <div>
+        <div class="home-top" style="background: url(https://cdn.jsdelivr.net/gh/Miazzy/xdata-stock-service@v1.0.3/src/assets/img/top_bg3.png) no-repeat; background-size: 100% 100%;">
+            
+        </div>
+        <div class="home-latestcompany" style="margin-bottom:50px;">
+            <div class="home-middle-title-wrap">
+                <div class="home-middle-title">企业注册信息</div>
+            </div>
+            <div class="home-middle-content"> <a href="/firm/f06eabb81ca5c48435a99d643e2aeb6d.html" class="a-decoration">
+                    <div class="list-item">
+                        <div class="list-item-top">
+                            <div class="list-item-logo"><img src="https://qccdata.qichacha.com/AutoImage/f06eabb81ca5c48435a99d643e2aeb6d.jpg" onerror="this.src='https://co-image.qichacha.com/CompanyImage/default.jpg'"></div>
+                            <div class="list-item-name">
+                                普洱蓝艺装饰工程有限责任公司
+                            </div>
+                        </div>
+                        <div class="list-item-middle">
+                            <div class="col-3">
+                                <div class="col-subtitle">
+                                    法定代表人
+                                </div> <object class="col-title"> <a class="text-primary" href="/people?name=%E9%99%88%E6%B5%A9&amp;keyno=f06eabb81ca5c48435a99d643e2aeb6d">陈浩</a> </object>
+                            </div>
+                            <div class="col-3">
+                                <div class="col-subtitle">注册资金</div>
+                                <div class="col-title"> 100万元人民币 </div>
+                            </div>
+                            <div class="col-3">
+                                <div class="col-subtitle">成立日期</div>
+                                <div class="col-title">2021-02-24</div>
+                            </div>
+                        </div>
+                    </div>
+                </a> <a href="/firm/d4f885e22664f9d052677b608c2b20af.html" class="a-decoration">
+                    <div class="list-item">
+                        <div class="list-item-top">
+                            <div class="list-item-logo"><img src="https://qccdata.qichacha.com/AutoImage/d4f885e22664f9d052677b608c2b20af.jpg" onerror="this.src='https://co-image.qichacha.com/CompanyImage/default.jpg'"></div>
+                            <div class="list-item-name">
+                                云南昌泰茶文化传播有限公司
+                            </div>
+                        </div>
+                        <div class="list-item-middle">
+                            <div class="col-3">
+                                <div class="col-subtitle">
+                                    法定代表人
+                                </div> <object class="col-title"> <a class="text-primary" href="/pl/pr69945dc0f6c3f2578f67e1abe225ef.html">孙荷丽</a> </object>
+                            </div>
+                            <div class="col-3">
+                                <div class="col-subtitle">注册资金</div>
+                                <div class="col-title"> 200万元人民币 </div>
+                            </div>
+                            <div class="col-3">
+                                <div class="col-subtitle">成立日期</div>
+                                <div class="col-title">2021-02-24</div>
+                            </div>
+                        </div>
+                    </div>
+                </a> <a href="/firm/e8f81f0e689e23a5619ea28b5c22fde3.html" class="a-decoration">
+                    <div class="list-item">
+                        <div class="list-item-top">
+                            <div class="list-item-logo"><img src="https://qccdata.qichacha.com/AutoImage/e8f81f0e689e23a5619ea28b5c22fde3.jpg" onerror="this.src='https://co-image.qichacha.com/CompanyImage/default.jpg'"></div>
+                            <div class="list-item-name">
+                                深圳市为盛生鲜连锁超市有限公司松元厦分店
+                            </div>
+                        </div>
+                        <div class="list-item-middle">
+                            <div class="col-3">
+                                <div class="col-subtitle">
+                                    负责人
+                                </div> <object class="col-title"> <a class="text-primary" href="/people?name=%E7%BD%97%E5%B0%8F%E7%8E%B2&amp;keyno=e8f81f0e689e23a5619ea28b5c22fde3">罗小玲</a> </object>
+                            </div>
+                            <div class="col-3">
+                                <div class="col-subtitle">注册资金</div>
+                                <div class="col-title"> —</div>
+                            </div>
+                            <div class="col-3">
+                                <div class="col-subtitle">成立日期</div>
+                                <div class="col-title">2021-02-24</div>
+                            </div>
+                        </div>
+                    </div>
+                </a> <a href="/firm/f4f90c5e89e59d98f9d176da0cbe6eb9.html" class="a-decoration">
+                    <div class="list-item">
+                        <div class="list-item-top">
+                            <div class="list-item-logo"><img src="https://qccdata.qichacha.com/AutoImage/f4f90c5e89e59d98f9d176da0cbe6eb9.jpg" onerror="this.src='https://co-image.qichacha.com/CompanyImage/default.jpg'"></div>
+                            <div class="list-item-name">
+                                成都国爱康养老服务有限公司
+                            </div>
+                        </div>
+                        <div class="list-item-middle">
+                            <div class="col-3">
+                                <div class="col-subtitle">
+                                    法定代表人
+                                </div> <object class="col-title"> <a class="text-primary" href="/people?name=%E5%BE%90%E8%89%B3&amp;keyno=f4f90c5e89e59d98f9d176da0cbe6eb9">徐艳</a> </object>
+                            </div>
+                            <div class="col-3">
+                                <div class="col-subtitle">注册资金</div>
+                                <div class="col-title"> 200万元人民币 </div>
+                            </div>
+                            <div class="col-3">
+                                <div class="col-subtitle">成立日期</div>
+                                <div class="col-title">2021-02-24</div>
+                            </div>
+                        </div>
+                    </div>
+                </a> <a href="/firm/674bf33aa1935c58c6233fc069c79f6d.html" class="a-decoration">
+                    <div class="list-item">
+                        <div class="list-item-top">
+                            <div class="list-item-logo"><img src="https://qccdata.qichacha.com/AutoImage/674bf33aa1935c58c6233fc069c79f6d.jpg" onerror="this.src='https://co-image.qichacha.com/CompanyImage/default.jpg'"></div>
+                            <div class="list-item-name">
+                                忠玉康旅房地产（云南）合伙企业（有限合伙）
+                            </div>
+                        </div>
+                        <div class="list-item-middle">
+                            <div class="col-3">
+                                <div class="col-subtitle">
+                                    执行事务合伙人
+                                </div> <object class="col-title"> <a class="text-primary" href="/people?name=%E9%99%88%E8%8E%89%E8%90%8D&amp;keyno=674bf33aa1935c58c6233fc069c79f6d">陈莉萍</a> </object>
+                            </div>
+                            <div class="col-3">
+                                <div class="col-subtitle">注册资金</div>
+                                <div class="col-title"> —</div>
+                            </div>
+                            <div class="col-3">
+                                <div class="col-subtitle">成立日期</div>
+                                <div class="col-title">2021-02-24</div>
+                            </div>
+                        </div>
+                    </div>
+                </a> <a href="/firm/e8b6f1042b4103adac29a45f62066f1f.html" class="a-decoration">
+                    <div class="list-item">
+                        <div class="list-item-top">
+                            <div class="list-item-logo"><img src="https://qccdata.qichacha.com/AutoImage/e8b6f1042b4103adac29a45f62066f1f.jpg" onerror="this.src='https://co-image.qichacha.com/CompanyImage/default.jpg'"></div>
+                            <div class="list-item-name">
+                                阳光保险代理有限公司四川成都分公司
+                            </div>
+                        </div>
+                        <div class="list-item-middle">
+                            <div class="col-3">
+                                <div class="col-subtitle">
+                                    负责人
+                                </div> <object class="col-title"> <a class="text-primary" href="/people?name=%E5%BC%A0%E5%B8%85&amp;keyno=e8b6f1042b4103adac29a45f62066f1f">张帅</a> </object>
+                            </div>
+                            <div class="col-3">
+                                <div class="col-subtitle">注册资金</div>
+                                <div class="col-title"> —</div>
+                            </div>
+                            <div class="col-3">
+                                <div class="col-subtitle">成立日期</div>
+                                <div class="col-title">2021-02-24</div>
+                            </div>
+                        </div>
+                    </div>
+                </a> <a href="/firm/3a7c3510501731470a2049f7bd49ab6d.html" class="a-decoration">
+                    <div class="list-item">
+                        <div class="list-item-top">
+                            <div class="list-item-logo"><img src="https://qccdata.qichacha.com/AutoImage/3a7c3510501731470a2049f7bd49ab6d.jpg" onerror="this.src='https://co-image.qichacha.com/CompanyImage/default.jpg'"></div>
+                            <div class="list-item-name">
+                                金安荟（深圳）投资合伙企业（有限合伙）
+                            </div>
+                        </div>
+                        <div class="list-item-middle">
+                            <div class="col-3">
+                                <div class="col-subtitle">
+                                    执行事务合伙人
+                                </div> <object class="col-title"> <a class="text-primary" href="/pl/pr09047beca11e42eecd7c07731bf7f1.html">唐闻宇</a> </object>
+                            </div>
+                            <div class="col-3">
+                                <div class="col-subtitle">注册资金</div>
+                                <div class="col-title"> 100万元人民币 </div>
+                            </div>
+                            <div class="col-3">
+                                <div class="col-subtitle">成立日期</div>
+                                <div class="col-title">2021-02-24</div>
+                            </div>
+                        </div>
+                    </div>
+                </a> <a href="/firm/696a7e8563a6a5e97b14ac61c7720969.html" class="a-decoration">
+                    <div class="list-item">
+                        <div class="list-item-top">
+                            <div class="list-item-logo"><img src="https://qccdata.qichacha.com/AutoImage/696a7e8563a6a5e97b14ac61c7720969.jpg" onerror="this.src='https://co-image.qichacha.com/CompanyImage/default.jpg'"></div>
+                            <div class="list-item-name">
+                                深圳市凌盈信息咨询有限公司
+                            </div>
+                        </div>
+                        <div class="list-item-middle">
+                            <div class="col-3">
+                                <div class="col-subtitle">
+                                    法定代表人
+                                </div> <object class="col-title"> <a class="text-primary" href="/pl/pr3b8b181788c0d5373c060ed0cd93c1.html">童高燕</a> </object>
+                            </div>
+                            <div class="col-3">
+                                <div class="col-subtitle">注册资金</div>
+                                <div class="col-title"> 100万元人民币 </div>
+                            </div>
+                            <div class="col-3">
+                                <div class="col-subtitle">成立日期</div>
+                                <div class="col-title">2021-02-24</div>
+                            </div>
+                        </div>
+                    </div>
+                </a> <a href="/firm/bfee790b5e7f165a8f59af408022b11f.html" class="a-decoration">
+                    <div class="list-item">
+                        <div class="list-item-top">
+                            <div class="list-item-logo"><img src="https://qccdata.qichacha.com/AutoImage/bfee790b5e7f165a8f59af408022b11f.jpg" onerror="this.src='https://co-image.qichacha.com/CompanyImage/default.jpg'"></div>
+                            <div class="list-item-name">
+                                成都李记老柴房竹苑里餐饮有限责任公司
+                            </div>
+                        </div>
+                        <div class="list-item-middle">
+                            <div class="col-3">
+                                <div class="col-subtitle">
+                                    法定代表人
+                                </div> <object class="col-title"> <a class="text-primary" href="/pl/prdbb4dde06df212cf577d12dee35a32.html">李绍鲜</a> </object>
+                            </div>
+                            <div class="col-3">
+                                <div class="col-subtitle">注册资金</div>
+                                <div class="col-title"> 80万元人民币 </div>
+                            </div>
+                            <div class="col-3">
+                                <div class="col-subtitle">成立日期</div>
+                                <div class="col-title">2021-02-24</div>
+                            </div>
+                        </div>
+                    </div>
+                </a> <a href="/firm/f8d59d89a5d05227303edde73a3e529c.html" class="a-decoration">
+                    <div class="list-item">
+                        <div class="list-item-top">
+                            <div class="list-item-logo"><img src="https://qccdata.qichacha.com/AutoImage/f8d59d89a5d05227303edde73a3e529c.jpg" onerror="this.src='https://co-image.qichacha.com/CompanyImage/default.jpg'"></div>
+                            <div class="list-item-name">
+                                云南照蔓诺商贸有限公司
+                            </div>
+                        </div>
+                        <div class="list-item-middle">
+                            <div class="col-3">
+                                <div class="col-subtitle">
+                                    法定代表人
+                                </div> <object class="col-title"> <a class="text-primary" href="/pl/pr476411f3ebafe75b5db9c27d4fb8e8.html">赵军波</a> </object>
+                            </div>
+                            <div class="col-3">
+                                <div class="col-subtitle">注册资金</div>
+                                <div class="col-title"> 100万元人民币 </div>
+                            </div>
+                            <div class="col-3">
+                                <div class="col-subtitle">成立日期</div>
+                                <div class="col-title">2021-02-24</div>
+                            </div>
+                        </div>
+                    </div>
+                </a> </div>
+        </div>
+        <div style="height:30px;"></div>
+    </div>
+    <tabbar></tabbar>
+</div>
+</template>
+
+<script>
+import {
+    ref,
+    reactive,
+    onMounted,
+    toRefs,
+    getCurrentInstance
+} from "vue";
+import {
+    useStore
+} from "vuex";
+import {
+    useRouter,
+    useRoute
+} from "vue-router";
+import tabbar from "@/components/tabbar";
+export default {
+    name: "home",
+    components: {
+        tabbar
+    },
+    setup(props, context) {
+        const {
+            ctx
+        } = getCurrentInstance();
+        const $store = useStore(); // ctx.$store === $store  ==>true 其实是同一个对象！
+        const $router = useRouter();
+        const $route = useRoute(); // 可以拿到所有和路由相关的参数
+        const active = ref("");
+        const timeData = ref(36000000);
+        const headerActive = ref(false);
+
+        const state = reactive({
+            homeImgs: [],
+            tabArray: []
+        });
+
+        const ball = reactive({
+            show: false,
+            el: ""
+        });
+
+        ctx.$http.get("http://test.happymmall.com/home/homeData").then(res => {
+            const {
+                images,
+                tabList
+            } = res.data;
+            state.tabArray = tabList;
+            state.homeImgs = images;
+        });
+
+        onMounted(() => {
+            ctx.$eventBus.$emit("changeTag", 0);
+            window.addEventListener("scroll", pageScroll);
+        });
+
+        const addToCart = (event, tag) => {
+            $store.commit("cart/addToCart", tag);
+            ball.show = true;
+            ball.el = event.target;
+        };
+
+        const beforeEnter = el => {
+            const dom = ball.el;
+            const rect = dom.getBoundingClientRect();
+            const x = rect.left - window.innerWidth * 0.6;
+            const y = -(window.innerHeight - rect.top);
+            el.style.display = "block";
+            el.style.transform = `translate3d(0,${y}px,0)`;
+            const inner = el.querySelector(".inner");
+            inner.style.transform = `translate3d(${x}px,0,0)`;
+        };
+
+        const enter = (el, done) => {
+            document.body.offsetHeight;
+            el.style.transform = "translate3d(0,0,0)";
+            const inner = el.querySelector(".inner");
+            inner.style.transform = "translate3d(0,0,0)";
+            el.addEventListener("transitionend", done);
+        };
+
+        const afterEnter = el => {
+            ball.show = false;
+            el.style.display = "none";
+        };
+
+        const handleClick = linkUrl => {
+            $router.push("/classify/product");
+        };
+
+        const pageScroll = () => {
+            const scrollTop =
+                window.pageYOffset ||
+                document.documentElement.scrollTop ||
+                document.body.scrollTop;
+            scrollTop > 100 ?
+                (headerActive.value = true) :
+                (headerActive.value = false);
+        };
+
+        return {
+            active,
+            timeData,
+            headerActive,
+            ...toRefs(ball),
+            ...toRefs(state),
+            addToCart,
+            beforeEnter,
+            enter,
+            afterEnter,
+            handleClick,
+            pageScroll
+        };
+    }
+};
+</script>
+<style lang="scss" scoped>
+@import "./home.css";
+</style>
