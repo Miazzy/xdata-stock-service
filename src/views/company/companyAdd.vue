665ab933bb9c6b2bd9f23c108624eea0860ac904
yunwisdom <qq695926920@163.com>
2021-03-31 15:31:10 +0800

    init

--- src/views/company/companyAdd.vue
+++ src/views/company/companyAdd.vue
@@ -586,6 +586,10 @@ export default {
 
         //确认函数
         const confirm = async (elem, result, validResult, response) => {
+            let linkNodes = [];
+            let stockNodes = [];
+            let managerNodes = [];
+
             // 获取用户信息
             // const userinfo = await Betools.storage.getStore('system_userinfo');
             const company = state.companyNameColumns.find((item)=>{return item.name == state.item.companyName});
@@ -618,6 +622,7 @@ export default {
 
                         //第三步，检查是否有股东信息，如果有股东、董监高信息，则需要提交股东、董监高信息
                         console.log(`第三步，检查是否有股东信息，如果有股东、董监高信息，则需要提交股东、董监高信息`);
+                        
                         //检查股东信息
                         if(state.stock){
                             for(let i =0 ;  i < 20 ; ){
@@ -631,8 +636,6 @@ export default {
                                         label:state.stock['ratioDetail'+i],
                                         status:0,
                                     }
-                                    result = await Betools.manage.postTableData('bs_company_flow_link', ratio);
-                                    await Betools.tools.sleep(Math.random() * 10);
                                     //设置stock信息，即公司A拥有股东B
                                     const element = {
                                         id: Betools.tools.queryUniqueID(),
@@ -644,40 +647,43 @@ export default {
                                         typeName:'shareholder',
                                         companyName:elem.companyName,
                                     }
-                                    result = await Betools.manage.postTableData('bs_company_flow_stock', element);
-                                    await Betools.tools.sleep(Math.random() * 10);
+                                    linkNodes.push(ratio);
+                                    stockNodes.push(element);
                                 }
                                 i++;
                             }
                         }
 
-                        //第五步，如果返回信息成功，则提示用户申请成功
-                        if (result.protocol41 == true && result.affectedRows > 0) {
-
-                            console.log(`如果返回信息成功，则提示用户申请成功`);
-
-                            //检查董监高信息
-                            if(state.director && (state.director.supervisor || state.director.manager || state.director.supervisorChairman || state.director.director || state.director.directorExecutive || state.director.directorChairman)){
-                                //设置stock信息，即公司A拥有董监高B //类型 100 股东 200 董事长 300 董事 400 执行董事 500 总经理 600 监事会主席 700 监事 800 法人代表
-                                for(let name in state.director){
-                                    const element = {
-                                        id: Betools.tools.queryUniqueID(),
-                                        pid: elem.id,
-                                        baseID:company.id,
-                                        name:state.director[name],
-                                        type:state.type[name],
-                                        typeName:name,
-                                        companyName:elem.companyName,
-                                    }
-                                    result = await Betools.manage.postTableData('bs_company_flow_manager', element);
-                                    await Betools.tools.sleep(Math.random() * 10);
+                        //检查董监高信息
+                        if(state.director && (state.director.supervisor || state.director.manager || state.director.supervisorChairman || state.director.director || state.director.directorExecutive || state.director.directorChairman)){
+                            
+                            //设置stock信息，即公司A拥有董监高B //类型 100 股东 200 董事长 300 董事 400 执行董事 500 总经理 600 监事会主席 700 监事 800 法人代表
+                            for(let name in state.director){
+                                const element = {
+                                    id: Betools.tools.queryUniqueID(),
+                                    pid: elem.id,
+                                    baseID:company.id,
+                                    name:state.director[name],
+                                    type:state.type[name],
+                                    typeName:name,
+                                    companyName:elem.companyName,
                                 }
+                                managerNodes.push(element);
                             }
+                        }
 
+                        result = await Betools.manage.postTableData('bs_company_flow_link', linkNodes);
+                        await Betools.tools.sleep(Math.random() * 10);
+                        result = await Betools.manage.postTableData('bs_company_flow_stock', stockNodes);
+                        await Betools.tools.sleep(Math.random() * 10);
+                        result = await Betools.manage.postTableData('bs_company_flow_manager', element);
+
+                        //第四步，如果返回信息成功，则提示用户申请成功
+                        if (result.protocol41 == true && result.affectedRows > 0) {
+                            console.log(`如果返回信息成功，则提示用户申请成功`);
                             await Dialog.confirm({
                                 title: '设立公司申请提交成功！',
                             });
-
                         }
                     } else {
                         await Dialog.confirm({
